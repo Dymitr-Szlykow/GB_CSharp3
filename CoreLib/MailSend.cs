6021b5ec318ba88e13ce57096ce20a698e886b1c
Dymitr-Szlykow <62315853+Dymitr-Szlykow@users.noreply.github.com>
2021-11-15 22:39:53 +0300

    Логика и первично действующее WPF приложение.
    - создана минимально работающая логика в отдельной библиотеке.
    - проверка адресов с помощью Regex.
    - первично работающее WPF приложение с одним окном;
      может отправлять письма на один адрес за раз.
    - зависимости через INotifyPropertyChanged.
    - нельзя менять отправителя.

---
+++ CoreLib/MailSend.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CoreLib
+{
+    public class MailSendCore
+    {
+        protected static Regex _MailAdressTemplate = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}");
+        protected static Regex _DeleteMe = new Regex("^[a-zA-Z]{1}[a-zA-Z0-9]{1,9}$");
+
+        public MailAddress Destination { get; protected set; }
+        public MailAddress Sender { get; protected set; }
+        public MailMessage message;  // TODO
+        protected SmtpClient smtpClient;
+
+        public string SmtpClientHost { get => smtpClient.Host; }
+        public int SmtpClientPort { get => smtpClient.Port; }
+
+
+
+        public MailSendCore()
+        {
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            smtpClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("somemail@gmail.com", "mypassword"),
+                EnableSsl = true
+            };
+        }
+
+        public MailSendCore(SmtpClient client)
+        {
+            smtpClient = client;
+        }
+
+
+        public bool SetSender(string address)
+        {
+            if (AddressFits(address))
+            {
+                Sender = new MailAddress(address);
+                return true;
+            }
+            else return false;
+        }
+
+        public bool SetSender(string address, string name)
+        {
+            if (AddressFits(address))
+            {
+                Sender = new MailAddress(address, name);
+                return true;
+            }
+            else return false;
+        }
+
+        public bool SetDestination(string address)
+        {
+            if (AddressFits(address))
+            {
+                Destination = new MailAddress(address);
+                return true;
+            }
+            else return false;
+        }
+
+        public bool SetSmtpClient()
+        {
+            // TODO
+            return false;
+        }
+
+
+        public bool AddressFits(string address) => _MailAdressTemplate.IsMatch(address);
+
+        public static (string, int) GetClient(string address)  // TODO
+        {
+            string[] temp = address.Split(new char[] { '@', '.' }, StringSplitOptions.None);
+            switch (temp[1])
+            {
+                case "yandex":
+                    return ("smtp.yandex.ru", 25);
+                case "gmail":
+                    return ("smtp.gmail.com", 58);
+                case "mail":
+                    return ("smtp.mail.ru", 25);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+
+
+        protected MailMessage FormMessage(string subject, string body)
+        {
+            return new MailMessage(Sender, Destination)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = false
+            };
+        }
+        protected MailMessage FormHTMLMessage(string subject, string body)
+        {
+            return new MailMessage(Sender, Destination)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+        }
+
+        protected void Send() => smtpClient.Send(message);  // TODO
+        public (MailMessage, string) Send(MailMessage message)
+        {
+            string report = $"! ПОПЫТКА ОТПРАВИТЬ ПИСЬМО !" +
+                $"\n\tна тему: \"{message.Subject}\"" +
+                $"\n\tадресат: {message.To}" +
+                $"\n\n\"" + message.Body +
+                $"\"\n\nЖелаете причинить соответствующие меры субъекту по адресу {message.From.Address}?";
+            //smtpClient.Send(message);
+            Debug.WriteLine(report);
+            return (message, report);
+        }
+        public string Send(string subject, string body) => Send(FormMessage(subject, body)).Dispose();
+        public string SendAsHTML(string subject, string body) => Send(FormHTMLMessage(subject, body)).Dispose();
+        public string SendMakingHTML(string subject, string body) => Send(FormHTMLMessage(subject, "<p>" + body + "</p>")).Dispose();
+    }
+
+    public static class Extensions
+    {
+        public static string Dispose(this (MailMessage, string) theTuple)
+        {
+            theTuple.Item1.Dispose();
+            return theTuple.Item2;
+        }
+    }
+}
---
+++ MailSend.WPFapp/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using CoreLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSend.WPFapp
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MailSendCore core;
+
+        private string _Destination;
+        private string _MailBody;
+        private string _Subject;
+
+        public string SenderAdress { get => core.Sender.Address; }
+        public string Client { get => core.SmtpClientHost; }
+        public string Port { get => core.SmtpClientPort.ToString(); }
+        public string Destination
+        {
+            get => _Destination;
+            set
+            {
+                _Destination = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string MailBody
+        {
+            get => _MailBody;
+            set
+            {
+                _MailBody = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Subject
+        {
+            get => _Subject;
+            set
+            {
+                _Subject = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            core = new MailSendCore();
+            core.SetSender("somemail@gmail.com", "Tom");
+            Destination = "somemail@yandex.ru";
+            Subject = "Тест";
+            MailBody = "Письмо-тест работы smtp-клиента.";
+            DataContext = this;
+        }
+
+
+
+        #region TODO
+        internal void btn_SwitchSender_Click(object sender, RoutedEventArgs e)
+        {
+            core.SetSender("адресс", "имя");
+        }
+
+        internal void btn_SwitchSmtpClient_Click(object sender, RoutedEventArgs e)
+        {
+            core.SetSmtpClient();
+        }
+
+        internal void btn_Send_Click(object sender, RoutedEventArgs e)
+        {
+            if (core.SetDestination(Destination))
+            {
+                string warning = core.SendMakingHTML(Subject, MailBody);
+                _ = MessageBox.Show(warning, "отправка письма", MessageBoxButton.YesNoCancel, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                _ = MessageBox.Show("некорректный адрес", "ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        #endregion
+    }
+}
